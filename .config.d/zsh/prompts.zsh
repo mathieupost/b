de6206132e5adc48e9235a2d325fdc354b32aed6
Burke Libbey <burke@burkelibbey.org>
2012-05-01 18:43:35 -0500

    Move prompt to C

--- .config.d/zsh/prompts.zsh
+++ .config.d/zsh/prompts.zsh
@@ -1,26 +1,4 @@
-function build-prompt() {
-  ret=$?
-  case "$ret" in
-    0) statcolor="%{$fg[green]%}" ;;
-    *) statcolor="%{$fg[red]%}$ret"   ;;
-  esac
-  git=$(/Users/burke/src/b/prompt/prompt)
-
-  turbo=""
-  if [[ $RUBY_HEAP_MIN_SLOTS == 1000000 ]] {
-    turbo="X "
-  }
-
-  case `hostname` in
-    burke.local) pathcolor="%{$fg[cyan]%}"    ;;
-    burke)       pathcolor="%{$fg[cyan]%}"    ;;
-    hoth)        pathcolor="%{$fg[yellow]%}"  ;;
-    *)           pathcolor="%{$fg[red]%}"     ;;
-  esac
-
-  echo "$pathcolor%c %{$fg[red]%}$turbo$git$statcolor%#%{$reset_color%} "
-}
-PROMPT='`build-prompt`'
+PROMPT='`/Users/burke/src/b/prompt/prompt $?`'
 
 [ x$TERM = "xeterm-color" ] && setopt singlelinezle
 [ x$TERM = "xdumb" ] && unsetopt zle && PS1='$ '
