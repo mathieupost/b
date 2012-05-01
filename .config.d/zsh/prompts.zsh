4207909e2ad11727d226919d275ad96e3f4ac2ec
Burke Libbey <burke@burkelibbey.org>
2012-05-01 16:24:19 -0500

    Fixed prompts

--- .config.d/zsh/prompts.zsh
+++ .config.d/zsh/prompts.zsh
@@ -1,33 +1,26 @@
 function build-prompt() {
-    ret=$?
-    case "$ret" in 
-        0) statcolor="%{$fg[green]%}" ;;
-        *) statcolor="%{$fg[red]%}$ret"   ;;
-    esac 
-    ruby=$(/Users/burke/src/b/prompt/ruby_info)
-    git=$(/Users/burke/src/b/prompt/prompt 2>/dev/null)
+  ret=$?
+  case "$ret" in
+    0) statcolor="%{$fg[green]%}" ;;
+    *) statcolor="%{$fg[red]%}$ret"   ;;
+  esac
+  git=$(/Users/burke/src/b/prompt/prompt 2>/dev/null)
 
-    turbo=""
-    if [[ $RUBY_HEAP_MIN_SLOTS == 1000000 ]] {
-      turbo="X"
-    }
+  turbo=""
+  if [[ $RUBY_HEAP_MIN_SLOTS == 1000000 ]] {
+    turbo="X "
+  }
 
-    case `hostname` in
-        espresso.local) pathcolor="%{$fg[cyan]%}"    ;;
-        espresso)       pathcolor="%{$fg[cyan]%}"    ;;
-        oglaroon)       pathcolor="%{$fg[yellow]%}"  ;;
-        li325-53)       pathcolor="%{$fg[magenta]%}" ;;
-        *)              pathcolor="%{$fg[red]%}"     ;;
-    esac
+  case `hostname` in
+    burke.local) pathcolor="%{$fg[cyan]%}"    ;;
+    burke)       pathcolor="%{$fg[cyan]%}"    ;;
+    hoth)        pathcolor="%{$fg[yellow]%}"  ;;
+    *)           pathcolor="%{$fg[red]%}"     ;;
+  esac
 
-    echo "$pathcolor%c %{$fg[red]%}$turbo$ruby $git $statcolor%#%{$reset_color%} "
+  echo "$pathcolor%c %{$fg[red]%}$turbo$git$statcolor%#%{$reset_color%} "
 }
 PROMPT='`build-prompt`'
-
-function build-rprompt() {
-  echo "%{$fg[black]%}%~ %l %h %*%{$fg[white]%}%{$fg[reset]%}"
-}
-# RPROMPT='`build-rprompt`'
 
 [ x$TERM = "xeterm-color" ] && setopt singlelinezle
 [ x$TERM = "xdumb" ] && unsetopt zle && PS1='$ '
