27a931c4ea28ef172c86c585811073af6be63e5f
Burke Libbey <burke@burkelibbey.org>
2012-05-01 19:17:51 -0500

    Add .vim to dotfiles

--- .config.d/zsh/zshrc.zsh
+++ .config.d/zsh/zshrc.zsh
@@ -24,10 +24,6 @@ P="$P:/usr/bin"
 # Tail of PATH
 export PATH="$P:$PATH"
 
-export USERWM=`which xmonad`
-
-export NODE_PATH="/usr/local/lib/node"
-
 # history
 HISTFILE=~/.zsh_history
 HISTSIZE=50000
@@ -45,52 +41,10 @@ setopt \
 fpath=($fpath $HOME/.config.d/zsh/func)
 typeset -U fpath
 
-#autoload -U promptinit
-#promptinit
-
-bindkey -v
-bindkey "^B" backward-char
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^F" forward-char
-bindkey "^X^F" vi-find-next-char
-bindkey "^N" down-line-or-history
-bindkey "^R" history-incremental-search-backward
-bindkey "^S" history-incremental-search-forward
-bindkey "^X^N" infer-next-history
-bindkey "^P" up-line-or-history
-bindkey "^H" backward-delete-char
-bindkey "^W" backward-kill-word
-bindkey "^X^J" vi-join
-bindkey "^K" kill-line
-bindkey "^X^K" kill-buffer
-bindkey "^U" kill-whole-line
-bindkey "^X^B" vi-match-bracket
-bindkey "^X^O" overwrite-mode
-bindkey "^V" quoted-insert
-bindkey "^T" transpose-chars
-bindkey "^Y" yank
-bindkey "^D" delete-char-or-list
-bindkey "^X*" expand-word
-bindkey "^XG" list-expand
-bindkey "^Xg " list-expand
-bindkey "^M" accept-line
-bindkey "^J" accept-line
-bindkey "^O" accept-line-and-down-history
-bindkey "^X^V" vi-cmd-mode
-bindkey "^L" clear-screen
-bindkey "^X^X" exchange-point-and-mark
-bindkey "^Q" push-line
-bindkey "^G" send-break
-bindkey "^@" set-mark-command
-bindkey "^Xu " undo
-bindkey "^X^U" undo
-bindkey "^_" undo
-
-
 autoload -Uz colors
 colors
 
+. ~/.config.d/zsh/keybindings.zsh
 . ~/.config.d/zsh/git.zsh
 . ~/.config.d/zsh/prompts.zsh
 . ~/.config.d/zsh/titles.zsh
