1d8acdee17f07ab8e1f1043307f60054ddd0b110
Burke Libbey <burke@burkelibbey.org>
2012-05-01 22:44:46 -0500

    Removed stuff pry does from irb configs

--- .config.d/ruby/all.rb
+++ .config.d/ruby/all.rb
@@ -1,4 +1,1 @@
-#require File.join(File.dirname(__FILE__), 'active_record_extensions')
-require File.join(File.dirname(__FILE__), 'method_inheritance')
-require File.join(File.dirname(__FILE__), 'local_methods')
 require File.join(File.dirname(__FILE__), 'time')
--- .config.d/ruby/local_methods.rb
+++
@@ -1,5 +0,0 @@
-class Object
-  def local_methods(obj=self)
-    (obj.methods - obj.class.superclass.instance_methods).sort
-  end
-end 
