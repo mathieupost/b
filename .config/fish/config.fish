82ea36d97a9d3e7a8928e9565ac27b2f75fd5795
Burke Libbey <burke@libbey.me>
2013-07-09 13:20:14 -0500

    no more fish greeting. it breaks scp and is big.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,6 @@
 function fish_prompt ; ~/src/b/prompt/prompt $status ; end
 
-set fish_greeting (set_color red)'                 ___
-   ___======____='(set_color yellow)'---='(set_color red)')
- /T            \_'(set_color yellow)'--==='(set_color red)')
- L \ '(set_color yellow)'(@)   '(set_color red)'\~    \_'(set_color yellow)'-=='(set_color red)')
-  \      / )J'(set_color yellow)'~~    '(set_color red)'\\'(set_color yellow)'-='(set_color red)')
-   \\\\___/  )JJ'(set_color yellow)'~~    '(set_color red)'\)
-    \_____/JJJ'(set_color yellow)'~~      '(set_color red)'\
-    / \  , \\'(set_color red)'J'(set_color yellow)'~~~~      \
-   (-\)'(set_color red)'\='(set_color yellow)'|  \~~~        L__
-   ('(set_color red)'\\'(set_color yellow)'\\)  ( -\)_            ==__
-    '(set_color red)'\V    '(set_color yellow)'\-'(set_color red)'\) =='(set_color yellow)'=_____  J\   \\\\
-           '(set_color red)'\V)     \_)'(set_color yellow)' \   JJ J\)
-                       /J J'(set_color red)'T'(set_color yellow)'\JJJ'(set_color red)'J)
-                       (J'(set_color yellow)'JJ| '(set_color red)'\UUU)
-                        (UU)'
+set fish_greeting ''
 
 function mac ; test (uname -s) = "Darwin" ; end
 function linux ; test (uname -s) = "Linux" ; end
