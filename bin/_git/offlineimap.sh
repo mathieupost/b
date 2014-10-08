7702ab3d84c907ab6043b2efdccf1aad5557a979
Burke Libbey <burke@libbey.me>
2014-10-08 01:53:32 -0400

    imap stuff

--- bin/_git/offlineimap.sh
+++ bin/_git/offlineimap.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
 export PYTHONPATH=/lib/python2.7/site-packages
+export PATH="/usr/local/bin:${PATH}"
+
+PID="$(cat ~/.offlineimap/pid)"
+ps aux | grep "[ ]${PID}" && kill "${PID}"
+
+opts="-q"
+if [[ "$(jot -r 1 1 10)" = "10" ]] ; then # 90% chance
+  opts=""
+fi
+
 if [ ! -f "/Users/burke/NOMAIL" ]; then
   mv /Users/burke/.mutt/offlineimap.log /Users/burke/.mutt/offlineimap.log.1
-  /bin/offlineimap 2>&1 | /usr/local/bin/ts > /Users/burke/.mutt/offlineimap.log
-  if [ ! $? -eq 0 ]; then
-    /usr/local/bin/terminal-notifier \
+  offlineimap ${opts} 2>&1 | ts > /Users/burke/.mutt/offlineimap.log
+  if [ $? -ne 0 ]; then
+    terminal-notifier \
       -title offlineimap \
       -group offlineimap \
       -message "Sync failed (touch ~/NOMAIL maybe?)" \
