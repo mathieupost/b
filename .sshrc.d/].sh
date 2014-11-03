61bbc2cf703d7a162f2e56230333289b91a9495c
Burke Libbey <burke@libbey.me>
2014-11-03 14:52:32 -0500

    ]

--- .sshrc.d/].sh
+++ .sshrc.d/].sh
@@ -1,18 +1,49 @@
 function ]gs() {
-  ]g "${HOME}/src/github.com/shopify" "$@"
+  _]g "${HOME}/src/github.com/shopify" "$@"
+  cd "${HOME}/src/github.com/shopify/${project}"
 }
 
 function ]gb() {
-  ]g "${HOME}/src/github.com/burke" "$@"
+  _]g "${HOME}/src/github.com/burke" "$@"
+  cd "${HOME}/src/github.com/burke/${project}"
 }
 
-function ]g() {
-  dir=$1;shift
+function ]hs() {
+  _]g "${HOME}/src/github.com/shopify" "$@"
+  open "https://github.com/Shopify/${project}"
+}
+
+function ]hb() {
+  _]g "${HOME}/src/github.com/burke" "$@"
+  open "https://github.com/burke/${project}"
+}
+
+function ]hsp() {
+  _]g "${HOME}/src/github.com/shopify" "$@"
+  open "https://github.com/Shopify/${project}/pulls"
+}
+
+function ]hbp() {
+  _]g "${HOME}/src/github.com/burke" "$@"
+  open "https://github.com/burke/${project}/pulls"
+}
+
+function ]hsm() {
+  _]g "${HOME}/src/github.com/shopify" "$@"
+  open "https://github.com/Shopify/${project}/pulls/burke"
+}
+
+function ]hbm() {
+  _]g "${HOME}/src/github.com/burke" "$@"
+  open "https://github.com/burke/${project}/pulls/burke"
+}
+
+function _]g() {
+  local dir=$1;shift
   if [[ $# -eq 0 ]]; then
     cd "${dir}"
     ls
     return
   fi
-  result="$(cd "${dir}" && find . -type d -maxdepth 1 -mindepth 1 | sed 's#./##' | matcher "$@" | head -1)"
-  cd "${dir}/${result}"
+  project="$(cd "${dir}" && find . -type d -maxdepth 1 -mindepth 1 | sed 's#./##' | matcher "$@" | head -1)"
 }
