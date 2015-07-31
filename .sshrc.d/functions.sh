190352933a4ec9810e2812499840d750c2098e64
Burke Libbey <burke@libbey.me>
2015-07-31 16:50:34 -0400

    oneline some stuff

--- .sshrc.d/functions.sh
+++ .sshrc.d/functions.sh
@@ -1,45 +1,19 @@
-e() {
-  $EDITOR "$@"
-}
-vpnc() {
-  osascript -e 'tell application "Viscosity" to connect "Chicago VPN"'
-}
-vpncd() {
-  osascript -e 'tell application "Viscosity" to disconnect "Chicago VPN"'
-}
-vpna() {
-  osascript -e 'tell application "Viscosity" to connect "Ashburn VPN"'
-}
-vpnad() {
-  osascript -e 'tell application "Viscosity" to disconnect "Ashburn VPN"'
-}
-sgb() {
-  git branch --list | grep -v '^\*' | selecta | xargs git checkout
-}
-gfrog() {
-  gfro $(gb | g '*' | ap2)
-}
-gug() {
-  guo $(gb | g '*' | ap2)
-}
-gufg() {
-  gufo $(gb | g '*' | ap2)
-}
-gfd() {
-  git fetch origin $(gb | g '*' | ap2) && git reset --hard FETCH_HEAD
-}
-ap() {
-  awk "{print \$$1}"
-}
-def() {
-  ag "def $@"
-}
+e()     { $EDITOR "$@"; }
+vpnc()  { osascript -e 'tell application "Viscosity" to connect "Chicago VPN"'; }
+vpncd() { osascript -e 'tell application "Viscosity" to disconnect "Chicago VPN"'; }
+vpna()  { osascript -e 'tell application "Viscosity" to connect "Ashburn VPN"'; }
+vpnad() { osascript -e 'tell application "Viscosity" to disconnect "Ashburn VPN"'; }
+sgb()   { git branch --list | grep -v '^\*' | selecta | xargs git checkout; }
+gfrog() { gfro $(gb | g '*' | ap2); }
+gug()   { guo $(gb | g '*' | ap2); }
+gufg()  { gufo $(gb | g '*' | ap2); }
+gfd()   { git fetch origin $(gb | g '*' | ap2) && git reset --hard FETCH_HEAD; }
+ap()    { awk "{print \$$1}"; }
+def()   { ag "def $@"; }
+psag()  { ps aux | g $1 | gvg; }
 cros_sdk() {
   cd ~/src/coreos
   vagrant ssh -c "coreos/chromite/bin/cros_sdk" || sh -c 'vagrant up && vagrant ssh -c coreos/chromite/bin/cros_sdk'
-}
-psag() {
-  ps aux | g $1 | gvg
 }
 ghp() {
   local pr_id="$(git symbolic-ref --short HEAD | sed 's/^pr\///')"
@@ -60,42 +34,18 @@ gac() {
     git commit -a -m "$*"
   fi
 }
-gh() {
-  cd $(_gh "$@")
-}
-ghs() {
-  cd $(_gh Shopify $1)
-}
-ghb() {
-  cd $(_gh burke $1)
-}
-fdg() {
-  find . | grep "$@"
-}
-gfr() {
-  git fetch "$@" && git reset --hard FETCH_HEAD
-}
-ggh() {
-  open $(git remote show -n origin | grep "github.com:" | head -1 | awk '{print $3}' | sed 's/:/\//' | sed 's#git@#https://#' | sed 's/\.git$//')
-}
-ghg() {
-  open "https://github.com/$1"
-}
-ghgb() {
-  ghg "burke/$1"
-}
-ghgs() {
-  ghg "Shopify/$1"
-}
-mi() {
-  gem install --no-ri --no-rdoc "$@" && rbenv rehash
-}
-gbt() {
-  git branch --track "$1" "origin/$1"
-}
-up() {
-  while [[ ! -d .git ]] && [[ $(pwd) != "/" ]]; do cd ".."; done
-}
+gh() { cd $(_gh "$@"); }
+ghs() { cd $(_gh Shopify $1); }
+ghb() { cd $(_gh burke $1); }
+fdg() { find . | grep "$@"; }
+gfr() { git fetch "$@" && git reset --hard FETCH_HEAD; }
+ggh() { open $(git remote show -n origin | grep "github.com:" | head -1 | awk '{print $3}' | sed 's/:/\//' | sed 's#git@#https://#' | sed 's/\.git$//'); }
+ghg() { open "https://github.com/$1"; }
+ghgb() { ghg "burke/$1"; }
+ghgs() { ghg "Shopify/$1"; }
+mi() { gem install --no-ri --no-rdoc "$@" && rbenv rehash; }
+gbt() { git branch --track "$1" "origin/$1"; }
+up() { while [[ ! -d .git ]] && [[ $(pwd) != "/" ]]; do cd ".."; done; }
 eachm() {
   # TODO: can't use matcher here because it returns even non-matches.
   #matcher --limit 100000 $1 < ~/.machines | xargs resolve_machine
