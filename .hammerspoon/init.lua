bf14d1ffafc1ad829a5ce3bce649450a7b8b32c1
Burke Libbey <burke@libbey.me>
2015-10-06 13:25:46 -0400

    hammerspooon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,6 @@
-local function setupSecondaryScreen()
-  local s = "Color LCD"
-  hs.layout.apply({
-    {"Slack",    nil, s, hs.geometry.rect(0, 0, 0.65, 1),        nil, nil},
-    {"Messages", nil, s, hs.geometry.rect(0.65, 0, 0.35, 0.5),   nil, nil},
-    {"Adium",    nil, s, hs.geometry.rect(0.65, 0.5, 0.35, 0.5), nil, nil},
-  })
-end
+-- vim: foldmethod=marker
 
+-- App hotkeys {{{
 local function hideshowhotkey(bundleID)
   return function()
     local fw = hs.window.focusedWindow()
@@ -38,7 +32,9 @@ for i, bundleID in pairs(bindings) do
     hs.hotkey.bind({"cmd", "ctrl"}, tostring(i), hideshowhotkey(bundleID))
   end
 end
+-- }}}
 
+-- Leader bindings {{{
 local leader = hs.hotkey.modal.new({"cmd"}, "`")
 function leader:bind1(k, f)
   self:bind({}, k, function() leader:exit(); f() end)
@@ -53,7 +49,17 @@ leader:bind1("1", setupSecondaryScreen)
 
 leader:bind1("r", hs.reload)
 leader:bind1("c", hs.toggleConsole)
+-- }}}
 
+-- Screen layout {{{
+local function setupSecondaryScreen()
+  local s = "Color LCD"
+  hs.layout.apply({
+    {"Slack",    nil, s, hs.geometry.rect(0, 0, 0.65, 1),        nil, nil},
+    {"Messages", nil, s, hs.geometry.rect(0.65, 0, 0.35, 0.5),   nil, nil},
+    {"Adium",    nil, s, hs.geometry.rect(0.65, 0.5, 0.35, 0.5), nil, nil},
+  })
+end
 
 local function screensChanged()
   local screens = hs.screen.allScreens()
@@ -64,3 +70,27 @@ end
 
 local screenWatcher = hs.screen.watcher.new(screensChanged)
 screenWatcher:start()
+-- }}}
+
+-- Caffeine {{{
+-- borrowed from https://github.com/cmsj/hammerspoon-config/blob/master/init.lua
+local caffeine = hs.menubar.new()
+
+function setCaffeineDisplay(state)
+  local result
+  if state then
+    result = caffeine:setIcon("caffeine-on.pdf")
+  else
+    result = caffeine:setIcon("caffeine-off.pdf")
+  end
+end
+
+function caffeineClicked()
+  setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+  caffeine:setClickCallback(caffeineClicked)
+  setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+-- }}}
