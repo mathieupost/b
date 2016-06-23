0f81b2521d17cab9637c14ef513a4e41e21c620c
Burke Libbey <burke@libbey.me>
2016-06-23 17:07:53 -0400

    whatever

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,7 +21,6 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
-
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡ '
 set __fish_git_prompt_char_stagedstate '→'
