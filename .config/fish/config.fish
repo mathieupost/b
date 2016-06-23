bc1b6a9159830f83d34cceca96d6572c096b91f7
Burke Libbey <burke@libbey.me>
2016-06-23 17:01:56 -0400

    more fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,25 +3,40 @@
 set fish_greeting ""
 
 
-# fish git prompt
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate 'x'
+set __fish_git_prompt_char_dirtystate '⚡ '
 set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 
 function fish_prompt
   set last_status $status
+
   set_color $fish_color_cwd
   printf '%s' (prompt_pwd)
   set_color normal
+
   printf '%s ' (__fish_git_prompt)
+
   set_color normal
 end
 
@@ -266,7 +281,6 @@ function _]g --argument-names 'dir'
   end
   set -g __g_project (find "$dir" -type d -maxdepth 1 -mindepth 1 | awk -F/ '{print $NF}' | matcher $argv | head -1)
 end
-
 
 
 if test -f /opt/dev/dev.fish
