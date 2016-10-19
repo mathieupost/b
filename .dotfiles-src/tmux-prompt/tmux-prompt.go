a8b440a4e2502ed0a56e1b35d69330ce52ec0228
Burke Libbey <burke@libbey.me>
2016-10-19 13:41:06 -0400

    stuff

--- .dotfiles-src/tmux-prompt/tmux-prompt.go
+++ .dotfiles-src/tmux-prompt/tmux-prompt.go
@@ -39,7 +39,6 @@ const (
 )
 
 func init() {
-	println("wtf")
 }
 
 var superscripts = []string{"⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"}
@@ -106,12 +105,10 @@ func main() {
 	if err != nil {
 		hn = "???"
 	}
-	println("a")
 
 	loadAvgs, err := loadAvg()
 	t2 := time.Now()
 
-	println("a")
 	home := os.Getenv("HOME")
 	pers, _ := ioutil.ReadDir(home + "/.mail/notify/p:INBOX/new")
 	shop, _ := ioutil.ReadDir(home + "/.mail/notify/s:INBOX/new")
@@ -122,7 +119,6 @@ func main() {
 	}
 	mails := fmt.Sprintf("%s %d:%d:%d", color, len(pers), len(shop), len(git))
 
-	println("a")
 	stat, err := os.Stat(home + "/.mutt/mbsync.log")
 	threshold := time.Now().Add(-3 * time.Minute)
 	if err != nil || stat.ModTime().Before(threshold) {
@@ -133,7 +129,6 @@ func main() {
 	batt := fmt.Sprintf("%d", C.percentage())
 	power := fmt.Sprintf("%0.1fW", float64(C.power())/1e6)
 
-	println("a")
 	u, s, _ := sampleCPU()
 
 	t4 := time.Now()
@@ -146,7 +141,6 @@ func main() {
 	if secs == -1 {
 		battRem = "⏳ "
 	}
-	println("a")
 
 	t5 := time.Now()
 	vpns := C.connected()
@@ -161,7 +155,6 @@ func main() {
 	vpnChunk := fmt.Sprintf("%sC%sA", chicagoColor, ashburnColor)
 	t6 := time.Now()
 
-	println("a")
 	fmt.Printf("%s",
 		nobold(otherBG, -1)+" "+
 			Arrow1+nobold(247, otherBG)+" "+
@@ -186,7 +179,6 @@ func main() {
 			" ")
 	t7 := time.Now()
 
-	println("a")
 	_ = t1
 	_ = t2
 	_ = t3
