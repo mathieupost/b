8b94c841bc5aeb1b82eaa95f8c1ec315e984f796
Burke Libbey <burke@libbey.me>
2017-08-23 14:02:34 -0400

    stuff

--- .dotfiles-src/hostname-fix/hostname-fix.c
+++ .dotfiles-src/hostname-fix/hostname-fix.c
@@ -1,7 +1,18 @@
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int
 main()
 {
-  return execl("/usr/sbin/scutil", "/usr/sbin/scutil", "--set", "HostName", "darmok.local", NULL);
+  pid_t pid;
+  int stat;
+
+  pid = fork();
+  if (pid == 0) {
+     return execl("/usr/sbin/scutil", "/usr/sbin/scutil", "--set", "HostName", "darmok.local", NULL);
+  }
+
+  waitpid(pid, &stat, 0);
+  return execl("/usr/sbin/scutil", "/usr/sbin/scutil", "--set", "LocalHostName", "darmok", NULL);
 }
