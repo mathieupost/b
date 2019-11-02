ec3682d278884f9edc22995a287873f79398e30d
Burke Libbey <burke@libbey.me>
2019-11-02 10:49:04 -0400

    Remove unused files

--- .dotfiles-src/tmux-prompt/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-buildGoModule rec {
-  name = "tmux-prompt-${version}";
-  version = "1.0";
-  modSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
-  src = ./.;
-}
--- .dotfiles-src/tmux-prompt/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/burke/dotfiles/dotfiles-src/tmux-prompt
-
-go 1.13
