e0289c2f599bfd1f2c62b2e17162c3eba56d7c45
Burke Libbey <burke@libbey.me>
2019-11-07 22:16:39 -0500

    user defaults

--- .config/nixpkgs/darwin-configuration.nix
+++ .config/nixpkgs/darwin-configuration.nix
@@ -8,6 +8,7 @@ in
   environment.systemPackages = with pkgs; [
     fzf
     git
+    ctags
     google-cloud-sdk
     htop
     jq
@@ -18,6 +19,33 @@ in
     tree
     zsh
   ];
+
+  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
+
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.orientation = "right";
+  system.defaults.dock.showhidden = true;
+  system.defaults.dock.mru-spaces = false;
+
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+
+  system.defaults.trackpad.Clicking = true;
+  system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
 
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin-configuration.nix";
 
