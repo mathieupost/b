ab009b4c05a9b7f7a95e9f83bab2adf437c340e9
Burke Libbey <burke@libbey.me>
2019-11-13 09:45:15 -0500

    stuff

--- src/burkeutils/default.nix
+++ src/burkeutils/default.nix
@@ -1,9 +1,14 @@
-{ stdenvNoCC ? (import <nixpkgs> {}).stdenvNoCC }:
+{ stdenvNoCC ? (import <nixpkgs> {}).stdenvNoCC
+, ruby       ? (import <nixpkgs> {}).ruby
+, bash       ? (import <nixpkgs> {}).bash
+}:
 stdenvNoCC.mkDerivation {
   pname = "burkeutils";
   version = "1.0.0";
 
   src = ./bin;
+
+  buildinputs = [ ruby bash ];
 
   installPhase = ''
     mkdir -p $out/bin
